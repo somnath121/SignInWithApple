307354348b429ac4cf1b102b178f2aac5060a5ee
Somnath Chatterjee <somnath.chatterjee121@gmail.com>
2019-09-27 18:03:21 +0700

    Commit

--- AppleLogin/ViewController.swift
+++ AppleLogin/ViewController.swift
@@ -7,14 +7,50 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
+import AuthenticationServices
+class ViewController: UIViewController,ASAuthorizationControllerDelegate,ASAuthorizationControllerPresentationContextProviding {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnLoginWithAppleDidTap(_ sender: Any) {
+        let loginRequest = ASAuthorizationAppleIDProvider().createRequest()
+        loginRequest.requestedScopes = [.fullName, .email]
+        
+        let controller = ASAuthorizationController(authorizationRequests: [loginRequest])
+        controller.delegate = self
+        controller.presentationContextProvider = self
+        controller.performRequests()
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization){
+        //if
+            let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential //{
+                // Create an account in your system.
+        let userIdentifier = appleIDCredential!.user
+        let userFirstName = appleIDCredential?.fullName?.givenName
+        let userLastName = appleIDCredential?.fullName?.familyName
+        let userEmail = appleIDCredential?.email
+            print("\(userIdentifier) ++ \(String(describing: userFirstName)) ++ \(String(describing: userLastName)) ++ \(String(describing: userEmail))")
+                
+           /* } else if let passwordCredential = authorization.credential as? ASPasswordCredential {
+                // Sign in using an existing iCloud Keychain credential.
+                let username = passwordCredential.user
+                let password = passwordCredential.password
+                print("\(username) ++ \(String(describing: password))")
+                //Navigate to other view controller
+            }*/
+        }
+    
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error){
+        print(error.localizedDescription)
+    }
 }
 
